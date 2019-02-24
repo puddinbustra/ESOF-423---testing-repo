fe3c54e57242ca279792814ed39d4c93cfcee5bb
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-02-23 20:20:18 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,9 @@
 from setuptools import find_packages
 from setuptools import setup
+from glob import glob
 
+setup(
+  name = "Happy"
+  packages=find_packages('src'),
+  package_dir={'': 'src'},
+)
