3db6f474a87073b3fa48ee40168d34affde83dba
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-03-01 16:51:27 -0700

    Update happy.py

--- src/mypkg/happy.py
+++ src/mypkg/happy.py
@@ -1,14 +1,23 @@
 
 
 
-def cake(input):
-    if int(input) == 10:
-        print("Happiness is here")
-        return 10
-    elif int(input) == 9:
-        try:
-            cake(3,5,4,6)
-        except:
-            print("I found an error, but I don't care")
-    else:
-        print("There is only sadness")
+# def cake(input):
+#     if int(input) == 10:
+#         print("Happiness is here")
+#         return 10
+#     elif int(input) == 9:
+#         try:
+#             cake(3,5,4,6)
+#         except:
+#             print("I found an error, but I don't care")
+#     else:
+#         print("There is only sadness")
+
+from __future__ import unicode_literals
+
+from django.test import TestCase
+
+# Create your tests here.
+#This is just to test travis. It's not actually testing anything significant. 
+def travisTest(num == 10):
+  return num
