2a9fc7f2fa7acc12198e1a8e5f4f741a0368b139
puddinbustra <43453214+puddinbustra@users.noreply.github.com>
2019-03-01 16:51:30 -0700

    Update test_cake.py

--- tests/test_cake.py
+++ tests/test_cake.py
@@ -1,7 +1,13 @@
-import pytest
-from mypkg.happy import cake
+# import pytest
+# from mypkg.happy import cake
 
 
-def test_fib_10():
-	assert(cake(10) == 10)
+# def test_fib_10():
+# 	assert(cake(10) == 10)
     
+    
+import pytest
+from prescriber.tests import travisTest
+#from file import function, for example: from prescriber.admin import adminFunction
+
+assert(travisTest(10) == 10)
