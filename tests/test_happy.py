15e4ed987b2d68621d38c79ae11c262e7592fa23
kinkalo <kvrosevelt@gmail.com>
2019-03-04 22:11:35 -0700

    Update test_happy.py

--- tests/test_happy.py
+++ tests/test_happy.py
@@ -9,5 +9,5 @@
 from happy import travisTest
 
 #from file import function, for example: from prescriber.admin import adminFunction
-def testing_happy():
+def nonsense():
     assert(travisTest(10) == 10)
