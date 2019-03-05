c7fa8b44cce56366606739eec5f401050fa53751
kinkalo <kvrosevelt@gmail.com>
2019-03-04 21:41:19 -0700

    Update happy.py

--- PsyGuideSite/happy.py
+++ PsyGuideSite/happy.py
@@ -20,5 +20,5 @@ import pytest
 
 # Create your tests here.
 #This is just to test travis. It's not actually testing anything significant. 
-def travisTest(num == 10):
+def travisTest(num):
   return num
